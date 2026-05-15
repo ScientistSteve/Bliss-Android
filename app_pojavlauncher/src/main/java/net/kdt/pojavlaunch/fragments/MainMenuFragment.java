c1b97ba447500860740954f1a1c63653cbaf63b7
ScientistSteve <rsen17306@gmail.com>
2026-05-15 16:01:58 +0530

    Fix missing MainMenuFragment tag and play icon drawable

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.java
@@ -45,6 +45,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainMenuFragment extends Fragment {
+    public static final String TAG = "MAIN_MENU_FRAGMENT";
     private mcVersionSpinner mVersionSpinner;
     private boolean isQuickActionsListMode = false;
     private boolean isServersCompactMode = false;
